0e62a3d7c5f003fcba4b7859192cd8a7ccc4400c
slawomir <slawomir.and1@gmail.com>
2021-07-09 17:01:06 +0200

    Django templates

---
+++ README.md
@@ -0,0 +1,1 @@
+My work through along the Python Django - The Practical Guide course from Udemy by Max Schwarzmuller.
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -18,24 +18,30 @@ monthly_challenges = {
 }
 
 def index(request):
-    list_items = ''
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse('month-challenge', args=[month])
-        list_items += f'<li><a href=\'{month_path}\'>{capitalized_month}</a></li>'
+    return render(request, 'challenges/index.html', {
+        'months': months
+    })
 
-    response_data = f'<ul>{list_items}</ul>'
-    return HttpResponse(response_data)
+    # list_items = ''
+    # for month in months:
+    #     capitalized_month = month.capitalize()
+    #     month_path = reverse('month-challenge', args=[month])
+    #     list_items += f'<li><a href=\'{month_path}\'>{capitalized_month}</a></li>'
+
+    # response_data = f'<ul>{list_items}</ul>'
+    # return HttpResponse(response_data)
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
+        return render(request, 'challenges/challenge.html', {
+            'text': challenge_text,
+            'month_name': month
+        })
     except:
         return HttpResponseNotFound('This month is not supported!')
-
-    return HttpResponse(challenge_text)
 
 
 def monthly_challenge_by_number(request, month):

a1837a1d922c50cb94b6c6f24f8243f13025d005
slawomir <slawomir.and1@gmail.com>
2021-07-20 22:48:37 +0200

    messing arround with data base

--- poletko/blog/views.py
+++ poletko/blog/views.py
@@ -1,63 +1,18 @@
 from django.shortcuts import render
 from datetime import date
 
+from .models import Post
+
 all_posts = [
-    {
-        'slug': 'my-traveling',
-        'image': 'traveler.jpg',
-        'author': 'Sławomir',
-        'date': date(2021, 7, 14),
-        'title': 'My travels',
-        'excerpt': 'There is nothing like travelinguuss!',
-        'content': '''
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste quod recusandae nobis vitae vero ducimus pariatur at facilis officiis, 
-            deleniti culpa mollitia tenetur maxime officia magni animi sed? Delectus, tenetur!
 
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste quod recusandae nobis vitae vero ducimus pariatur at facilis officiis, 
-            deleniti culpa mollitia tenetur maxime officia magni animi sed? Delectus, tenetur!
-        '''
-    },
-    {
-        'slug': 'my-vegies',
-        'image': 'traveler.jpg',
-        'author': 'Sławomir',
-        'date': date(2021, 7, 14),
-        'title': 'My vegies',
-        'excerpt': 'There is nothing like traveling!',
-        'content': '''
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste quod recusandae nobis vitae vero ducimus pariatur at facilis officiis, 
-            deleniti culpa mollitia tenetur maxime officia magni animi sed? Delectus, tenetur!
-
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste quod recusandae nobis vitae vero ducimus pariatur at facilis officiis, 
-            deleniti culpa mollitia tenetur maxime officia magni animi sed? Delectus, tenetur!
-        '''
-    },
-    {
-        'slug': 'my-money',
-        'image': 'traveler.jpg',
-        'author': 'Sławomir',
-        'date': date(2021, 7, 14),
-        'title': 'My money',
-        'excerpt': 'There is nothing like traveling!',
-        'content': '''
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste quod recusandae nobis vitae vero ducimus pariatur at facilis officiis, 
-            deleniti culpa mollitia tenetur maxime officia magni animi sed? Delectus, tenetur!
-
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste quod recusandae nobis vitae vero ducimus pariatur at facilis officiis, 
-            deleniti culpa mollitia tenetur maxime officia magni animi sed? Delectus, tenetur!
-        '''
-    },
 ]
-
 
 def get_date(post):
     return post['date']
 
 
 def starting_page(request):
-    # wybiera trzy ostatnie elementy listy, sprawdź o co chodzi !
-    sorted_posts = sorted(all_posts, key=get_date)
-    latests_posts = sorted_posts[-3:]
+    latests_posts = Post.objects.all().order_by('-date')[:3]
     return render(request, 'blog/index.html', {
         'posts': latests_posts
     })

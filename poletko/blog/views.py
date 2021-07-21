029c539df24ab5a6ec3df1a49fe884363257ed76
slawomir <slawomir.and1@gmail.com>
2021-07-21 16:48:53 +0200

    working with templates - connecting with database

--- poletko/blog/views.py
+++ poletko/blog/views.py
@@ -1,11 +1,6 @@
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
-
-def get_date(post):
-    return post['date']
-
 
 def starting_page(request):
     latests_posts = Post.objects.all().order_by('-date')[:3]
@@ -15,14 +10,15 @@ def starting_page(request):
 
 
 def posts(request):
-    all_posts = Post.objects.all().order_by
+    all_posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/all-posts.html', {
         'posts': all_posts
     })
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post-detail.html', {
-        'post': identified_post
+        'post': identified_post,
+        'post_tags': identified_post.tags.all()
     })
